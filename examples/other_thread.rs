b73ccd1d9935b8cc021550aecdf4af4eef4284ad
Christian Jordan <svorre2304@gmail.com>
2022-07-23 22:47:22 +0200

    Temp modification of other_thread example
    Modifies the example to test the validity of the generated backtrace(s).

--- examples/other_thread.rs
+++ examples/other_thread.rs
@@ -13,33 +13,43 @@ fn bar() {
     baz()
 }
 fn baz() {
-    println!("Hello from thread!");
+    //println!("Hello from thread!");
     // Sleep for simple sync. Can't read thread that has finished running
     //std::thread::sleep(std::time::Duration::from_millis(1000));
     loop {
         print!("");
-    };
+    }
 }
 
-
 fn main() {
-    let thread = std::thread::spawn(|| {
-        worker();
-    });
-    let os_handle = thread.as_raw_handle();
-    
-    // Allow the thread to start
-    std::thread::sleep(std::time::Duration::from_millis(100));
-
-    let mut frames = Vec::new();
-    unsafe {
-        backtrace::trace_thread_unsynchronized(os_handle,|frame| {
-            frames.push(BacktraceFrame::from(frame.clone()));
-            true
+    for index in 0..100 {
+        //std::thread::sleep_ms(1000);
+        let thread = std::thread::spawn(|| {
+            worker();
         });
-    }
+        let os_handle = thread.as_raw_handle();
 
-    let mut bt = Backtrace::from(frames);
-    bt.resolve();
-    println!("{:?}", bt);
+        // Allow the thread to start
+        std::thread::sleep(std::time::Duration::from_millis(500));
+
+        let mut frames = Vec::new();
+        unsafe {
+            backtrace::trace_thread_unsynchronized(os_handle, |frame| {
+                frames.push(BacktraceFrame::from(frame.clone()));
+                true
+            });
+        }
+        let len = frames.len();
+        let mut bt = Backtrace::from(frames);
+        bt.resolve();
+
+        if let Some(symbol) = bt.frames().last().unwrap().symbols()[0].name() {
+            let name = symbol.to_string();
+            if name != "RtlUserThreadStart" || index == 0 {
+                println!("{index}:{}, {:?}", len, bt);
+            }
+        } else {
+            panic!("no good");
+        }
+    }
 }
